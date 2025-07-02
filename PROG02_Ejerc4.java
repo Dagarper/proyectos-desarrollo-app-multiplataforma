fcb03e3b4b2e43bfbb74286e385a74950e7e5ab7
Dagarper <122533658+Dagarper@users.noreply.github.com>
2025-07-02 13:08:00 +0200

    Add files via upload

---
+++ PROG02_Ejerc4.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prog02_ejerc4;
+
+/**
+ *
+ * @author Yo
+ */
+public class PROG02_Ejerc4 {
+
+    public static void main(String[] args) {
+        int edad = 17;
+       String mayedad = edad >=18? "mayor":"menor";
+        
+       
+        
+        
+        System.out.println("La persona es " + mayedad + " de edad.");
+    }
+}
